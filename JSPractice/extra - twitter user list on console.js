d0035af043f70ac22a82e2199e5af87f7260e43c
kumankoishi.py <7iggif@naver.com>
2020-12-26 13:32:41 +0900

    Modify styles by script

--- JSPractice/extra - twitter user list on console.js
+++ JSPractice/extra - twitter user list on console.js
@@ -1,4 +1,5 @@
 var f = document.getElementsByClassName("css-1dbjc4n r-j7yic r-qklmqi r-1adg3ll r-1ny4l3l");
+var alluserstr = '';
 for (var i = 0; i < f.length; i++) {
     var element = f[i].getElementsByClassName("css-901oao css-16my406 r-1qd0xha r-ad9z0x r-bcqeeo r-qvutc0")[2];
 
@@ -8,6 +9,6 @@ for (var i = 0; i < f.length; i++) {
 
     var id = element.textContent;
     if (id[0] == '@') {
-        console.log(id);
+        alluserstr += (id + ' ');
     }
 }
